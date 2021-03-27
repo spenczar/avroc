7ef91c8dba1651a42a8028e1a27deb1d1cdc6aa3
Spencer Nelson <swnelson@uw.edu>
2021-03-26 21:32:49 -0700

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -3,9 +3,27 @@ from setuptools import setup
 docs_require = ['sphinx']
 publish_require = ['twine']
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="avroc",
-    version="0.1.0",
+    version="0.2.0",
+    description="A library for fast Avro serialization and deserialization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Spencer Nelson",
+    author_email="s@spencerwnelson.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    project_urls={
+        "Bug Tracker": "https://github.com/spenczar/avroc/issues",
+        "Documentation": "https://avroc.readthedocs.io/",
+        "Repository": "https://github.com/spenczar/avroc",
+    },
     packages=[
         "avroc",
         "avroc.bin",
